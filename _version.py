1dc7a0985b93e2f78d6cddd8301b80558cd2fe73
kpracchia <kevin.pracchia@gmail.com>
2020-12-08 23:10:57 +0100

    clone from pip3 install git+https://github.com/HeaTTheatR/KivyMD.git

---
+++ _version.py
@@ -0,0 +1,5 @@
+# THIS FILE IS GENERATED FROM KIVYMD SETUP.PY
+__version__ = "0.104.2.dev0"
+__hash__ = "4b4208ecab196d4a10e1ef2f231e629def62ee8c"
+__short_hash__ = "4b4208e"
+__date__ = "2020-12-08"
